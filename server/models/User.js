40e1df434d19d8bbcb2f4dbf4b0440d5a48c5a56
a1 <sa-fa@mail.ru>
2026-01-05 16:44:44 +0300

    загрузка файла

--- server/models/User.js
+++ server/models/User.js
@@ -1,12 +1,14 @@
-const { Schema, model, ObjectId} = require("mongoose")
+const { Schema, model, ObjectId } = require("mongoose");
+const mongoose = require("mongoose");
 
-const User = new Schema({
-    email: {type: String, unique: true, required: true},
-    password: {type: String, required: true},
-    diskSpace: {type: Number, default: 1024**3*10},
-    usedSpace: {type: Number, default: 0},
-    avatar: {type: String},
-    files: [{type: ObjectId, ref: "File"}],
-})
+const UserSchema = new Schema({
+    email: { type: String, unique: true, required: true },
+    password: { type: String, required: true },
+    diskSpace: { type: Number, default: 1024 ** 3 * 10 },
+    usedSpace: { type: Number, default: 0 },
+    avatar: { type: String },
+    files: [{ type: ObjectId, ref: "File" }],
+});
 
-module.exports = model("User", User)
+// Проверяем, существует ли модель уже
+module.exports = mongoose.models.User || model("User", UserSchema);
